e00001706feaaa1971e935d39f2ca31108eceab3
SteppCB <152199708+SteppCB@users.noreply.github.com>
2024-10-22 11:23:41 -0400

    Delete logical_operators_4.py

--- logical_operators_4.py
+++
@@ -1,9 +0,0 @@
-# use the logical operator not if statements
-
-# or: should return true if either a or b are truthy. False, if both are false
-def or_operator(a, b):
-  return
-
-# and: should return true only if both a and b are truthy
-def and_operator(a, b):
-  return
