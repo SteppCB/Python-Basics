41fc97296b156aaadda44cb1cf95a72b9aa38e69
SteppCB <152199708+SteppCB@users.noreply.github.com>
2024-10-24 12:04:54 -0400

    Update strings_2.py

--- python-basics-fall-2024-SteppCB-main/strings_2.py
+++ python-basics-fall-2024-SteppCB-main/strings_2.py
@@ -1,25 +1,14 @@
-'''
-reverse_string: you should be able to reverse a string
-  Example: "abc" => "cba"
-'''
-def reverse_string(str):
-  '''
-  * can be achieved by indexing the string using [] and :: operators
-  * can be done in one line
-  '''
-  return
+# reverse_string: you should be able to reverse a string
+# Example: "abc" => "cba"
+def reverse_string(s):
+    return s[::-1]  # Reverses the string using slicing
 
+# capitalize: should return the input in all-caps
+# Example: "this is a string" => "THIS IS A STRING"
+def capitalize(s):
+    return s.upper()  # Converts the string to uppercase
 
-'''
-capitalize: should return the input in all-caps
-  Example: "this is a string" => "THIS IS A STRING"
-'''
-def capitalize(str):
-  return
-
-'''
-split_string: should divide a string into substrings and return an list
-  Example: "Jane,Doe,21"  =>  [ "Jane", "Doe", "21" ]
-'''
-def split_string(str, splitAt = ','):
-  return
+# split_string: should divide a string into substrings and return a list
+# Example: "Jane, Doe, 21" => ["Jane", "Doe", "21"]
+def split_string(s, splitAt=','):
+    return s.split(splitAt)  # Splits the string at the given delimiter
