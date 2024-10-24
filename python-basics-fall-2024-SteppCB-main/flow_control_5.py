4808e9d61f944498e000d9b6c97fbebd4c095985
SteppCB <152199708+SteppCB@users.noreply.github.com>
2024-10-24 12:02:40 -0400

    Update flow_control_5.py

--- python-basics-fall-2024-SteppCB-main/flow_control_5.py
+++ python-basics-fall-2024-SteppCB-main/flow_control_5.py
@@ -1,10 +1,14 @@
-'''
-  Write a function that receives a number as its argument;
-  if the number is divisible by 3, the function should return 'fizz'
-  if the number is divisible by 5, the function should return 'buzz'
-  if the number is divisible by 3 and 5, the function should return 'fizzbuzz'
-  if the value provided was a number but doesn't match any of those criteria, return the number as is.
-  if no number was provided or if the value provided wasn't a number, return False.
-'''
 def fizz_buzz(num):
-  return
+    # Check if num is either an integer or a float
+    if type(num) not in [int, float]:
+        return False
+
+    # Apply the fizzbuzz logic
+    if num % 3 == 0 and num % 5 == 0:
+        return 'fizzbuzz'
+    elif num % 3 == 0:
+        return 'fizz'
+    elif num % 5 == 0:
+        return 'buzz'
+    else:
+        return num
