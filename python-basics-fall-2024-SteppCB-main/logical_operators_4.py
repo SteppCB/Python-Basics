01ad6c17b8233f1b719b09dbed775af3751c9bde
SteppCB <152199708+SteppCB@users.noreply.github.com>
2024-10-24 12:05:19 -0400

    Update logical_operators_4.py

--- python-basics-fall-2024-SteppCB-main/logical_operators_4.py
+++ python-basics-fall-2024-SteppCB-main/logical_operators_4.py
@@ -1,9 +1,7 @@
-# use the logical operator not if statements
-
 # or: should return true if either a or b are truthy. False, if both are false
 def or_operator(a, b):
-  return
+    return a or b
 
 # and: should return true only if both a and b are truthy
 def and_operator(a, b):
-  return
+    return a and b
